7060f6c62bc94fe16c096516e3d61470f721deda
Filipe S. <filipeschausst@gmail.com>
2025-09-28 20:59:13 -0300

    Preparação para a compilação do jogo para web

--- jogos/filipe/codigo/main.c
+++ jogos/filipe/codigo/main.c
@@ -1,21 +1,32 @@
+/* 
+    Exemplo de código web:
+    https://github.com/raysan5/raylib/blob/d97e0a8ac7ac150585e697344bf625e1583fe91c/examples/core/core_basic_window_web.c
+*/
+
 #include "raylib.h"
+#include <emscripten/emscripten.h>
 
 #define SW 1280
 #define SH 720
 
-int main(void) {
+void desenharFrame();
+
+int main() {
     
     InitWindow(SW, SH, "FrontEnd Bros.");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        
-        EndDrawing();
-    }
+    emscripten_set_main_loop(desenharFrame, 0, 1);
 
     CloseWindow();
     return 0;
+}
+
+void desenharFrame(){
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+        
+    EndDrawing();
 }
--- jogos/filipe/linux_build.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cc codigo/main.c -o frontBros \
-    -I/home/filipe/Documentos/Projetos/Jogos_Raylib/raylib_linux/include \
-    -L/home/filipe/Documentos/Projetos/Jogos_Raylib/raylib_linux/lib \
-    -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
