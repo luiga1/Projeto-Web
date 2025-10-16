7498b9fab3c1ef224ad509ca1d004a2bb7e2fd41
Filipe S. <filipeschausst@gmail.com>
2025-10-16 19:08:07 -0300

    Agora o <aside> pode ser aberto e fechado

---
+++ js/aside.js
@@ -0,0 +1,31 @@
+function esconde_aside(){
+
+    document.querySelector('#aside').style.display = 'none';
+
+    document.querySelector('main').style.marginLeft = '20px';
+
+    console.log("Aside escondido");
+}
+
+function mostrar_aside(){
+
+    let largura = window.innerWidth;
+
+    const aside = document.querySelector('#aside');
+
+    if(window.getComputedStyle(aside).getPropertyValue('display') == 'none'){
+
+        aside.style.display = 'flex';
+
+        if(largura < 790){
+            document.querySelector('main').style.marginLeft = '20px';
+        }else{
+            document.querySelector('main').style.marginLeft = '260px';
+        }
+
+        console.log("Aside mostrado");
+
+    }else{
+        esconde_aside();
+    }
+}
