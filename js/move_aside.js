9672a57ecdc78ed33473061f9d2909ff5fbc602d
artur732 <silveiraartur777@gmail.com>
2025-11-19 16:48:03 -0300

    Add files via upload

---
+++ js/move_aside.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById('butao');
+const azedo = document.getElementById('azedu');
+const body = document.body;
+
+botao.addEventListener('click', () => {
+
+    azedo.classList.toggle('hidden');
+    
+});
